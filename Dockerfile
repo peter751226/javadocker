e61bf7b72d48a77add45ab55ce79442faeb705f0
lzh <sdfs>
2017-03-23 20:15:11 -0700

    v1
    type

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM java:7
+COPY javadocker.java .
+RUN javac javadocker.java
+
+CMD["java","javadocker"]
---
+++ javadocker.java
@@ -0,0 +1,5 @@
+public class javadocker{
+   public static void main(String [] arg){
+     System.out.println("java docker 1");
+}
+}
