b2c022acd61163a2b9e6a5568583ec4c4305cc96
peter li <peter751226@gmail.com>
2017-03-24 05:09:56 +0000

    add in ubuntu

--- javadocker.java
+++ javadocker.java
@@ -1,5 +1,8 @@
 public class javadocker{
    public static void main(String [] arg){
+	
      System.out.println("java docker 1");
+     System.out.println("add 1");
+     System.out.println("add 2 ");
 }
 }
