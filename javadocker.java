7fde950a5125ba631bd6ae0f99ec2f3165a17c4e
peter li <peter751226@gmail.com>
2017-03-24 05:37:57 +0000

    3

--- javadocker.java
+++ javadocker.java
@@ -2,7 +2,6 @@ public class javadocker{
    public static void main(String [] arg){
 	
      System.out.println("java docker 1");
-     System.out.println("add 1");
      System.out.println("add 2 ");
 }
 }
